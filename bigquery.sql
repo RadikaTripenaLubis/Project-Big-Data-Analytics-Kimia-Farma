fa770cb2d9cad6fb038235637a262b86aab6c1f2
RadikaTripenaLubis <lubisradikatripena.com>
2025-08-04 09:08:11 +0700

    first commit

---
+++ bigquery.sql
@@ -0,0 +1,39 @@
+SELECT
+  ft.transaction_id,
+  ft.date,
+  kc.branch_id,
+  kc.branch_name,
+  kc.kota,
+  kc.provinsi,
+  kc.rating AS rating_cabang,
+  ft.customer_name,
+  p.product_id,
+  p.product_name,
+  p.price AS actual_price,
+  ft.discount_percentage,
+  
+  -- Persentase gross laba berdasarkan harga
+  CASE
+    WHEN p.price <= 50000 THEN 0.10
+    WHEN p.price <= 100000 THEN 0.15
+    WHEN p.price <= 300000 THEN 0.20
+    WHEN p.price <= 500000 THEN 0.25
+    ELSE 0.30
+  END AS persentase_gross_laba,
+  -- Harga setelah diskon
+  p.price * (1 - ft.discount_percentage / 100) AS nett_sales,
+  
+  -- Keuntungan bersih
+  p.price * (1 - ft.discount_percentage / 100) * 
+    CASE
+      WHEN p.price <= 50000 THEN 0.10
+      WHEN p.price <= 100000 THEN 0.15
+      WHEN p.price <= 300000 THEN 0.20
+      WHEN p.price <= 500000 THEN 0.25
+      ELSE 0.30
+    END AS nett_profit,
+  ft.rating AS rating_transaksi
+FROM
+  rakamin-kf-analytics-467805.kimia_farma.kf_final_transaction ft
+JOIN rakamin-kf-analytics-467805.kimia_farma.kf_kantor_cabang kc ON ft.branch_id = kc.branch_id
+JOIN rakamin-kf-analytics-467805.kimia_farma.kf_product p ON ft.product_id = p.product_id
